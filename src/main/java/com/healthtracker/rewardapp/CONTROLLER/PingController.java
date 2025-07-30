3604ed58c3e32d2ea4869545ef7c6057e66bb4d6
Mrunmayee <mrunmayee.njadhav@gmail.com>
2025-07-31 00:26:30 +0530

    Updated PingController for Backend uptime

---
+++ src/main/java/com/healthtracker/rewardapp/CONTROLLER/PingController.java
@@ -0,0 +1,18 @@
+package com.healthtracker.rewardapp.CONTROLLER;
+
+import com.healthtracker.rewardapp.REPO.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+    @RestController
+    public class PingController {
+        @Autowired
+        private UserRepository userRepository;
+
+        @GetMapping("/api/ping")
+        public String ping() {
+            long userCount = userRepository.count();  // lightweight DB call
+            return "pong - users in DB: " + userCount;
+        }
+    }
+}
