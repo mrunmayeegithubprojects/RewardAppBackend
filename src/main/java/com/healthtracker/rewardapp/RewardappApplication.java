abfa032c02b7a7d5942a78ff227673914724bbc6
Mrunmayee <mrunmayee.njadhav@gmail.com>
2025-11-06 23:17:02 +0530

    Redis cache setups

--- src/main/java/com/healthtracker/rewardapp/RewardappApplication.java
+++ src/main/java/com/healthtracker/rewardapp/RewardappApplication.java
@@ -2,10 +2,18 @@ package com.healthtracker.rewardapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class RewardappApplication {
-
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		return template;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(RewardappApplication.class, args);
 	}
---
+++ src/main/java/com/healthtracker/rewardapp/Service/CacheService.java
@@ -0,0 +1,25 @@
+package com.healthtracker.rewardapp.Service;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CacheService {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public CacheService(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void set(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+}
